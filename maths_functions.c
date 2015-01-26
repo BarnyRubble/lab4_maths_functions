a6dfac106063668ab3f792e3b4141bdb1e23c9e3
Barnaby Stewart <p14167376@myemail.dmu.ac.uk>
2015-01-26 13:12:42 +0000

    Created new repository for Lab 4

---
+++ maths_functions.c
@@ -0,0 +1,37 @@
+// FILE: maths_functions.c
+
+int add_values (int n1, int n2)
+{
+	return n1 + n2;
+}
+
+int subtract_values (int n1, int n2)
+{
+	return n1 - n2;
+}
+
+int multiply_values (int n1, int n2)
+{
+	return n1 * n2;
+}
+
+int divide_values (int n1, int n2)
+{
+	return n1 / n2;
+}
+
+int mod_values (int n1, int n2)
+{
+	return mod (n1, n2);
+}
+
+int trivial_function (int n1, int n2)
+{
+	return n1 + n2 * 2;
+}
+
+int reverse_mod_values (int n1, int n2)
+{
+	return mod (n2, n1);
+}
+
